50199f41834cd7c47705231960c0d948641e612f
Evan <evanknight18@gmail.com>
2023-06-29 18:34:54 -0600

    index.js files in controllers and models setup

---
+++ controllers/index.js
@@ -0,0 +1,5 @@
+const userController = require('./user-controller');
+const thoughtController = require('./thought-controller');
+const reactionController = require('./reaction-controller');
+
+module.exports = { userController, thoughtController, reactionController };
